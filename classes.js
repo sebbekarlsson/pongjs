f676e4e885f6f37b4c4dcac3adb88a12042e9566
Sebastian Karlsson <ianertson@gmail.com>
2021-06-18 11:31:59 +0200

    collisions

--- classes.js
+++ classes.js
@@ -1,7 +1,17 @@
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomDirection() {
+  return getRandomNumber(0, 3) === 0 ? -1 : 1;
+}
+
 class GameObject {
   constructor(x, y, width, height, ctx, color) {
     this.x = x;
     this.y = y;
+    this.dx = 0;
+    this.dy = 0;
     this.width = width;
     this.height = height;
     this.ctx = ctx;
@@ -10,6 +20,29 @@ class GameObject {
 
   update() {}
 
+  defaultUpdate() {
+    this.x += this.dx;
+    this.y += this.dy;
+  }
+
+  checkCollision(exclude) {
+    for (let i = 0; i < gameObjects.length; i++) {
+      const obj = gameObjects[i];
+
+      if (obj.constructor.name === exclude)
+        continue;
+
+      if (this.x + this.width >= obj.x && this.x <= obj.x + obj.width) {
+        if (this.y + this.height >= obj.y && this.y <= obj.y + obj.height) {
+          return true;
+        }
+      }
+    }
+  }
+
+  // rack upp handen nar den spammar ut
+  // objekt i consolen
+
   draw() {}
 }
 
@@ -17,9 +50,30 @@ class GameObject {
 class Ball extends GameObject {
   constructor(x, y, width, height, ctx, color) {
     super(x, y, width, height, ctx, color); // kalla pa foralderns konstruktor.
+    this.dir = getRandomDirection();
   }
 
-  update() {}
+  invertDirection() {
+    this.dir = this.dir * -1;
+  }
+
+  update() {
+    this.defaultUpdate();
+
+    if (this.x < 0) {
+      this.invertDirection();
+    }
+
+    if (this.x > 640) {
+      this.invertDirection();
+    }
+
+    if (this.checkCollision("Ball")) {
+      this.invertDirection();
+    }
+
+    this.dx = this.dir;
+  }
 
   draw() {
     this.ctx.beginPath();
@@ -29,4 +83,16 @@ class Ball extends GameObject {
  }
 }
 
-// rack upp handen nar ni ser en cirkel
+
+class Pad extends GameObject {
+  constructor(x, y, width, height, ctx, color) {
+    super(x, y, width, height, ctx, color); // kalla pa foralderns konstruktor.
+  }
+
+  update() {}
+
+  draw() {
+    this.ctx.fillStyle = this.color;  // doppa penseln i farg
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
--- main.js
+++ main.js
@@ -1,15 +1,53 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d"); // pappret vi ritar pa.
 
+const WIDTH = canvas.width;
+const HEIGHT = canvas.height;
 
-
-ball = new Ball(
-  120,    // x
-  120,    // y
-  32,     // width
-  32,     // height
+const ball = new Ball(
+  WIDTH / 2,    // x
+  HEIGHT / 2,    // y
+  16,     // width
+  16,     // height
   ctx,    // papper
   "white"// farg
 );
+gameObjects.push(ball); // tryck in i listan
 
-ball.draw();
+const pad1 = new Pad(
+  WIDTH-64,       // x
+  (HEIGHT / 2) - (86/2),   // y
+  32,           // width
+  86,           // height
+  ctx,          // papper
+  "white"       // farg
+);
+gameObjects.push(pad1); // tryck in i listan
+
+const pad2 = new Pad(
+  32,       // x
+  (HEIGHT / 2) - (86/2),   // y
+  32,           // width
+  86,           // height
+  ctx,          // papper
+  "white"       // farg
+);
+gameObjects.push(pad2); // tryck in i listan
+
+function loop() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, WIDTH, HEIGHT); // "rensar skarmen"
+
+  ball.update();
+  ball.draw();
+
+  pad1.update();
+  pad1.draw();
+
+  pad2.update();
+  pad2.draw();
+
+  requestAnimationFrame(loop);
+}
+
+loop();
